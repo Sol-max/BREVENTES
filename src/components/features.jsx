f0f564bf06e97cd7a4fbc6ba647b7eed99d78c71
Aleksandr Klenin <aleksandr.klenin@gmail.com>
2025-06-03 12:13:12 +0000

    fixed text width

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -4,12 +4,13 @@ export const Features = (props) => {
   return (
     <div id="features" className="text-center">
       <div className="container">
-        <div className="col-md-10 col-md-offset-1 section-title">
-          <h2>Teenused</h2>
-        </div>
         <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
+          <div className="col-md-10 col-md-offset-1 section-title">
+            <h2>Teenused</h2>
+          </div>
+          <div className="row">
+            {props.data
+              ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
                   {" "}
                   <i className={d.icon}></i>
@@ -17,9 +18,11 @@ export const Features = (props) => {
                   <p>{d.text}</p>
                 </div>
               ))
-            : "Loading..."}
+              : "Loading..."}
+          </div>
         </div>
       </div>
-    </div>
-  );
+      </div>
+      );
+      
 };
